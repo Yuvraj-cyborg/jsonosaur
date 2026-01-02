3f1d35b0da8bbb267cfc73f297678ddecb2283fc
Yuvraj <yuvrajbiswalofficial@gmail.com>
2026-01-02 23:41:31 +0530

    feat: array and string initialisation

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<stddef.h>
+#include<stdlib.h>
+#include<string.h>
 
 typedef enum{
   JsonNum,
@@ -10,15 +13,63 @@ typedef enum{
   JsonArr,
 }jsonType;
 
+typedef struct {
+  char *data;
+  size_t length;
+}jsonString;
+
+typedef struct jsonNode jsonNode;   
+
+typedef struct {
+  jsonNode *data;
+  size_t length;
+}jsonArray;
+
 typedef union{
-  int jsonInt;
-  char jsonStr[30];
+  double jsonNum;
+  jsonString jsonStr;
+  jsonArray jsonArr;
 }jsonValue;
 
-struct jsonNode {
+struct jsonNode{
   jsonType type;
   jsonValue value;
 };
+
+jsonNode create_str(char *data, size_t len) {
+    jsonString loc_str;
+    loc_str.data = malloc(len);
+    loc_str.length = len;
+    memcpy(loc_str.data,data,len);
+    jsonNode new_node;
+    new_node.type = JsonStr;
+    new_node.value.jsonStr = loc_str;
+    
+    return new_node;
+}
+
+jsonNode create_arr(size_t len) {
+  jsonArray loc_arr;
+  loc_arr.data = malloc(len * sizeof(jsonNode));
+  loc_arr.length = len;
+  jsonNode new_node;
+  new_node.type = JsonArr;
+  new_node.value.jsonArr = loc_arr;
+  return new_node;
+}
+
+void json_free(jsonNode *node) {
+  if (node==NULL) return;
+
+  switch(node->type){
+    case JsonStr: free(node->value.jsonStr.data);
+                  break;
+    case JsonArr: for(int i=0; i>)
+                    json_free(&element);
+                  free(array.data);
+    default: break;
+  }
+}
 
 int main() {
    printf("Hello World\n");
